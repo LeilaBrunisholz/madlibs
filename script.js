258a39b178d4d69fa8f4d21a78209adf3dd55e50
Leila Brunisholz <lhb22920@gmail.com>
2023-12-19 14:36:22 -0500

    added story

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ let noun = prompt('enter a noun:')
 let adverb = prompt ('enter an adverb:')
 let verb = prompt ('enter a noun:')
 
-let story = `once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+
+let story = `Once upon a time, on a ${colorful adjective} Christmas Eve, all the little ${animals} were tucked into their cozy ${noun}. They dreamed of magical ${plural noun} and giggled about the ${adjective} surprises Santa would leave under the ${noun}.
+
+Suddenly, Santa's reindeer started ${verb} on the rooftop, making a ${adjective} noise. The children jumped out of bed and peeked through the ${noun} to catch a glimpse of Santa in his ${colorful adjective} suit.
+
+To their surprise, Santa was doing the ${silly action} dance with his elves! They were all wearing ${silly clothing} and singing ${silly song lyrics}.
+
+Giggling, the kids joined the festive fun, and Santa handed each of them a special ${noun} filled with ${favorite treat}. It turned out, Santa loved a good ${favorite silly activity} just as much as the little ones.
+
+With hearts full of ${happy emotion}, the children and Santa danced into the night, creating the most ${adjective} holiday memories ever. And so, every Christmas, they looked forward to another round of silliness with Santa and his magical crew.`
+
+
+
+
+
+`
+
 
 
 let madLibOutputDiv = document.getElementById('madLibOutput')
